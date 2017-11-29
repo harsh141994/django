1c965cf8b595f414181eb6d54a1f08e951d28dde
Harsh <harsh141994@gmail.com>
2017-11-29 19:11:04 +0530

    menus app created

---
+++ src/menus/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+from django.db import models
+
+from restaurants.models import RestaurantLocation
+
+class Item(models.Model):
+    # associations
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL)#every item has an user
+    restaurant      = models.ForeignKey(RestaurantLocation)    #every item has a restaurant
+    # item stuff
+    name            = models.CharField(max_length=120)
+    contents        = models.TextField(help_text='Separate each item by comma')
+    excludes        = models.TextField(blank=True, null=True, help_text='Separate each item by comma')
+    public          = models.BooleanField(default=True)
+    timestamp       = models.DateTimeField(auto_now_add=True)
+    updated         = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated', '-timestamp']
+
+    def get_contents(self):
+        return self.contents.split(",")#returning the list of contents of the current(self) object(item)
+
+    def get_excludes(self):
+        return self.excludes.split(",")
--- src/muypicky/settings/base.py
+++ src/muypicky/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurants', #created a new app and added models so need to add in here
+    'menus',
 ]
 
 MIDDLEWARE = [
